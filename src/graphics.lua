b575008457bd9a490f8674e1e3da2ddd4acb2688
DEVensiv <devensiv@gmail.com>
2019-09-14 12:48:19 +0200

    test

--- src/graphics.lua
+++ src/graphics.lua
@@ -494,36 +494,36 @@ end
 
 function graphics.drawLocalGateInfo(x, y, address, energy, iris, err)
   gpu.setBackground(CYAN)
-  gpu.fill(x, y, 34, 13, " ")
+  gpu.fill(x, y, 34, 10, " ")
   gpu.setBackground(BACKGROUND)
-  gpu.fill(x+1, y+3, 32, 16, " ")
+  gpu.fill(x+1, y+3, 32, 13, " ")
   gpu.setBackground(CYAN)
   gpu.setForeground(TEXT_WHITE)
   gpu.set(x + 17 - string.len("LOCAL GATE DATA")/2, y+1, "LOCAL GATE DATA")
   gpu.setBackground(BACKGROUND)
   
   if energy == nil then
-    gpu.set(x+2, y+2, "AVAILABLE ENERGY: N/A")
+    gpu.set(x+2, y+4, "AVAILABLE ENERGY: N/A")
   else
-    gpu.set(x+2, y+2, "AVAILABLE ENERGY: " .. math.floor(energy + 0.5))
+    gpu.set(x+2, y+4, "AVAILABLE ENERGY: " .. math.floor(energy + 0.5))
   end
   
   if iris == nil then
-    gpu.set(x+2, y+2, "IRIS STATE: UNKNOWN")
+    gpu.set(x+2, y+5, "IRIS STATE: UNKNOWN")
   else
-    gpu.set(x+2, y+2, "IRIS STATE: " .. iris)
+    gpu.set(x+2, y+5, "IRIS STATE: " .. iris)
   end
   
-  gpu.set(x+2, y+8, "LAST DIALLING ERROR:")
-  gpu.fill(x+2, y+9, 31, 5, " ")
+  gpu.set(x+2, y+6, "LAST DIALLING ERROR:")
+  gpu.fill(x+2, y+7, 31, 5, " ")
   if err == nil then
     gpu.setForeground(GREEN)
-    gpu.set(x+2, y+10, "None")
+    gpu.set(x+2, y+8, "None")
   else
     gpu.setForeground(RED_DARK)
     local j = 0
     for i=1, #err, 30 do
-        gpu.set(x+2, y+10+j, err:sub(i,i+29))
+        gpu.set(x+2, y+8+j, err:sub(i,i+29))
         j = j + 1
     end
     
