cc5809b7abc0aaaf9de683e83f2059022fa8a9ed
DEVensiv <devensiv@gmail.com>
2019-09-14 13:08:17 +0200

    test

--- src/app.lua
+++ src/app.lua
@@ -113,11 +113,11 @@ end
 updateTerminateBtn()
 
 -- autoclose field
-local autocloseFld = AddressField.new(4, 43, 34, 5, "ENTER ADDRESS", CYAN, TEXT_WHITE, BACKGROUND)
+local autocloseFld = AddressField.new(4, 39, 34, 5, "ENTER ADDRESS", CYAN, TEXT_WHITE, BACKGROUND)
 autocloseFld:draw()
 
 -- configuration button
-local configBtn = Button.new(4, 43 + 5, 34, 3, "CONFIG", CYAN, TEXT_WHITE)
+local configBtn = Button.new(4, 39 + 6, 34, 3, "CONFIG", CYAN, TEXT_WHITE)
 configBtn:draw()
 
 -- IDC field
--- src/graphics.lua
+++ src/graphics.lua
@@ -471,13 +471,14 @@ end
 
 function graphics.drawRemoteInfo(x, y, address, energy)
   gpu.setBackground(CYAN)
-  gpu.fill(x, y, 34, 9, " ")
+  gpu.fill(x, y, 34, 8, " ")
   gpu.setBackground(BACKGROUND)
-  gpu.fill(x+1, y+2, 32, 7, " ")
+  gpu.fill(x+1, y+2, 32, 4, " ")
   gpu.setBackground(CYAN)
   gpu.setForeground(TEXT_WHITE)
   gpu.set(x + 17 - string.len("REMOTE GATE DATA")/2, y+1, "REMOTE GATE DATA")
   gpu.setBackground(BACKGROUND)
+
   if address == nil or string.len(address) < 7 then
     gpu.set(x+2, y+4, "ADDRESS: INVALID")
   else
@@ -496,34 +497,34 @@ function graphics.drawLocalGateInfo(x, y, address, energy, iris, err)
   gpu.setBackground(CYAN)
   gpu.fill(x, y, 34, 10, " ")
   gpu.setBackground(BACKGROUND)
-  gpu.fill(x+1, y+2, 32, 8, " ")
+  gpu.fill(x+1, y+2, 32, 6, " ")
   gpu.setBackground(CYAN)
   gpu.setForeground(TEXT_WHITE)
   gpu.set(x + 17 - string.len("LOCAL GATE DATA")/2, y+1, "LOCAL GATE DATA")
   gpu.setBackground(BACKGROUND)
   
   if energy == nil then
-    gpu.set(x+2, y+4, "AVAILABLE ENERGY: N/A")
+    gpu.set(x+2, y+2, "AVAILABLE ENERGY: N/A")
   else
-    gpu.set(x+2, y+4, "AVAILABLE ENERGY: " .. math.floor(energy + 0.5))
+    gpu.set(x+2, y+2, "AVAILABLE ENERGY: " .. math.floor(energy + 0.5))
   end
   
   if iris == nil then
-    gpu.set(x+2, y+6, "IRIS STATE: UNKNOWN")
+    gpu.set(x+2, y+3, "IRIS STATE: UNKNOWN")
   else
-    gpu.set(x+2, y+6, "IRIS STATE: " .. iris)
+    gpu.set(x+2, y+3, "IRIS STATE: " .. iris)
   end
   
-  gpu.set(x+2, y+8, "LAST DIALLING ERROR:")
-  gpu.fill(x+2, y+9, 31, 5, " ")
+  gpu.set(x+2, y+4, "LAST DIALLING ERROR:")
+  gpu.fill(x+2, y+5, 31, 5, " ")
   if err == nil then
     gpu.setForeground(GREEN)
-    gpu.set(x+2, y+10, "None")
+    gpu.set(x+2, y+6, "None")
   else
     gpu.setForeground(RED_DARK)
     local j = 0
     for i=1, #err, 30 do
-        gpu.set(x+2, y+10+j, err:sub(i,i+29))
+        gpu.set(x+2, y+6+j, err:sub(i,i+29))
         j = j + 1
     end
     
